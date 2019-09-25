790e2e9881762b1963674c3a826347319d312a02
liamiam <liam@liamiam.com>
2019-09-25 13:18:01 -0700

    Travis Test Commit 4

--- .travis.sh
+++ .travis.sh
@@ -1,1 +1,9 @@
 cmake CMakeLists.txt
+make
+echo Test.txt is here! > test.txt
+./xorenc encrypt test.txt
+./xorenc decrypt archive.aiz
+if [[ "$(cat test.txt)" != "$(cat archive.bin)" ]]; then
+    exit 1
+fi
+exit 0
